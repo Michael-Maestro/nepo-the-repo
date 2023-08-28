54d1234bc690ea9f60edd129fabc98f5a7337c6a
Michael-Maestro <142762275+Michael-Maestro@users.noreply.github.com>
2023-08-28 13:31:14 +1200

    Add files via upload

---
+++ mike.py
@@ -0,0 +1,19 @@
+"""
+def print_name(namer):
+    print("Hello", namer, ":)")
+print_name(input("What's YO NAME CUZ: "))
+"""
+def triangle_area(a,b):
+    return (0.5*a*b)
+print (triangle_area(5,10))
+def add_to_list(c):
+    my_list = [1,2,3,4]
+    print(my_list)
+    my_list.append(int(c))
+    return (my_list)
+print(add_to_list(input("awesome: ")))
+def square_number(n):
+    return ((int(n)*int(n)))
+
+print(square_number(input("t: ")))
+
