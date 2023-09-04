bf4828f22e8492f11492d528c066644172c16f1e
Michael-Maestro <142762275+Michael-Maestro@users.noreply.github.com>
2023-09-04 14:37:30 +1200

    Add files via upload
    FING

---
+++ newfree.py
@@ -0,0 +1,150 @@
+"""
+DO NOT MAKE CODE LIKE THIS YOU ARE INSANE!!!!!!!!!!!!
+"""
+
+# DO NOT
+def print_arbitrary():
+    t = input("what letter you HATE! ")
+    x = ("abcdefghijklmnopqrstuvwxyz")
+    if t == ("a"):
+        x = ("bcdefghijklmnopqrstuvwxyz")
+        print ("OK! THIS IS LFBET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("b"):
+        x = ("acdefghijklmnopqrstuvwxyz")
+        print ("OK! THIS IS ALFAET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("c"):
+        x = ("abdefghijklmnopqrstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("d"):
+        x = ("abcefghijklmnopqrstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPI LETTER", x)
+    else:
+        pass
+    if t == ("e"):
+        x = ("abcdfghijklmnopqrstuvwxyz")
+        print ("OK! THIS IS ALFABT WITHOUT STUPID LTTR", x)
+    else:
+        pass
+    if t == ("f"):
+        x = ("abcdeghijklmnopqrstuvwxyz")
+        print ("OK! THIS IS ALABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("g"):
+        x = ("abcdefhijklmnopqrstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("h"):
+        x = ("abcdefgijklmnopqrstuvwxyz")
+        print ("OK! THIS IS ALFABET WITOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("i"):
+        x = ("abcdefghjklmnopqrstuvwxyz")
+        print ("OK! THS S ALFABET WITHOUT STUPD LETTER", x)
+    else:
+        pass
+    if t == ("j"):
+        x = ("abcdfghiklmnopqrstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("k"):
+        x = ("abcdefghijlmnopqrstuvwxyz")
+        print ("O! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("l"):
+        x = ("abcdefghijkmnopqrstuvwxyz")
+        print ("OK! THIS IS AFABET WITHOUT STUPID ETTER", x)
+    else:
+        pass
+    if t == ("m"):
+        x = ("abcdefghijklnopqrstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("n"):
+        x = ("abcdefghijklmopqrstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("o"):
+        x = ("abcdefghijklmnpqrstuvwxyz")
+        print ("K! THIS IS ALFABET WITHUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("p"):
+        x = ("abcdefghijklmnoqrstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUID LETTER", x)
+    else:
+        pass
+    if t == ("q"):
+        x = ("abcdefghijklmnprstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("r"):
+        x = ("abcdefghijklmnpqstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTE", x)
+    else:
+        pass
+    if t == ("s"):
+        x = ("abcdefghijklmnopqrtuvwxyz")
+        print ("OK! THI I ALFABET WITHOUT TUPID LETTER", x)
+    else:
+        pass
+    if t == ("t"):
+        x = ("abcdefghijklmnpqrsuvwxyz")
+        print ("OK! HIS IS ALFABE WIHOU SUPID LEER", x)
+    else:
+        pass
+    if t == ("u"):
+        x = ("abcdefghijklmnopqrstvwxyz")
+        print ("OK! THIS IS ALFABET WITHOT STPID LETTER", x)
+    else:
+        pass
+    if t == ("v"):
+        x = ("abcdefghijklmnpqrstuvwxyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("w"):
+        x = ("abcdefghijklmnpqrstuvxyz")
+        print ("OK! THIS IS ALFABET ITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("x"):
+        x = ("abcdefghijklmnpqrstuvwyz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("y"):
+        x = ("abcdefghijklmnpqrstuvwxz")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == ("z"):
+        x = ("abcdefghijklmnpqrstuvwxy")
+        print ("OK! THIS IS ALFABET WITHOUT STUPID LETTER", x)
+    else:
+        pass
+    if t == (""):
+        return ("screw you")
+print_arbitrary()
+# Code below is actually readable ????? What????
+def inputter():
+    alfabet = ("abcdefghijklmnopqrstuvwxyz")
+    t = input("disliked letter here: ")
+    print ("ok! here you go:", alfabet.replace(t,''))     
+
+inputter()
+
+    
