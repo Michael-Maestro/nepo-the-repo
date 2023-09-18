47289e55a98ca846203cde97709ee25235cca3d8
Michael-Maestro <142762275+Michael-Maestro@users.noreply.github.com>
2023-09-18 15:05:53 +1200

    Update SOLID.py

--- SOLID.py
+++ SOLID.py
@@ -1,17 +1,17 @@
 """
-SOLID is a set of principles for writing better software code. It helps make code easier to understand, change, and maintain over time. 
-Each letter in "SOLID" stands for a principle:
-
-S: Single Responsibility Principle (SRP) - Each part of the code should do only one thing.
-O: Open/Closed Principle (OCP) - Code should be open to adding new features but closed to changing existing ones.
-L: Liskov Substitution Principle (LSP) - Substituting one part of code with another shouldn't break anything.
-I: Interface Segregation Principle (ISP) - Code should use small and specific parts, rather than big and general ones.
-D: Dependency Inversion Principle (DIP) - High-level parts of code shouldn't depend directly on low-level parts.
-
-SOLID is important because it helps create code that's easier to work with, adaptable to changes, and less likely to have errors. 
-It promotes good practices for designing software that's flexible, maintainable, and of high quality.
+SOLID is a set of principles that are used in coding.
+SOLID is in itself a word that states multiple different terms with their own meanings.
+1 - The S in SOLID stands for the Single Responsibility Principle - "A module should be responsible to one, and only one, actor." 
+What this infers is that modules (pieces of code) should only be allowed to do what is asked of them.
+2 - The O in SOLID stands for the Open-closed Principle - "software entities (classes, modules, functions, etc.) should be open for extension, but closed for modification."
+This means that code can be freely added to without having to alter the source code.
+3 - THE L in SOLID stands for the Liskov substitution principle -  "Functions that use pointers or references to base classes must be able to use objects of derived classes without knowing it."
+I don't know what a pointer is, but it's pretty important.
+4 - The I in SOLID stands for the Interface segregation principle - "Clients should not be forced to depend upon interfaces that they do not use."
+This simply means everything should have its place in essence; people shouldn't have to rely on other pieces of technology to achieve their unrelated end.
+5 - The D in SOLID stands for the Dependency inversion principle - "Depend upon abstractions, [not] concretions."
+This means that modules and code should depend on abstractions of code instead of non-changeable 'bad' code.
 """
-
 
 # SOLID: 
 # Single Responsibility Principle (SRP):
