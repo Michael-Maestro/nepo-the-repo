8c95f0a9ce5c4efc1502d7770ba93086d3d034fb
Michael-Maestro <142762275+Michael-Maestro@users.noreply.github.com>
2023-09-18 15:15:47 +1200

    Add files via upload

---
+++ trianglemaker.py
@@ -0,0 +1,20 @@
+"""
+def print_name(namer):
+    print("Hello", namer, ":)")
+print_name(input("What's YO NAME CUZ: ")) 
+"""
+def triangle_area(a,b): #This thing will math out a triangle's area for you. It probably listens to S of SOLId, but I couldn't be too sure.
+    return (0.5*a*b)
+print (triangle_area(5,10))
+def add_to_list(c):
+    my_list = [1,2,3,4]
+    print(my_list)
+    my_list.append(int(c))
+    return (my_list)
+print(add_to_list(input("awesome: ")))
+def square_number(n):
+    return ((int(n)*int(n)))
+
+print(square_number(input("t: ")))
+
+# tr
